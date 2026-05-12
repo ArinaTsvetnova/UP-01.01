5b4ba7febdcd2831924cdf7189020d1031d4a31c
lerat <cvetnovaarina@gmail.com>
2026-05-12 22:02:44 +0300

    Подключение базы данных

---
+++ UP01.01/Core.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UP01._01
+{
+    internal class Core
+    {
+        public static Zwe_UP0101Entities Context = new Zwe_UP0101Entities();
+    }
+}
